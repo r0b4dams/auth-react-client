3281264980fd70c47cbf9599923e06e8a3362411
Robert Adams <r0b4dams@gmail.com>
2024-01-31 13:40:27 -0800

    Create default provider config

---
+++ src/lib/AuthClient/defaults.ts
@@ -0,0 +1,10 @@
+export const DEFAULTS = {
+    SCOPE: 'openid offline_access',
+    // ACCESS_TOKEN_EXPIRE_WINDOW: 30000, // 30 sec window before making network refresh call
+  
+    LOGIN_PATH: '/auth/login',
+    LOGOUT_PATH: '/auth/logout',
+    REFRESH_PATH: '/auth/refresh',
+    REGISTER_PATH: '/auth/register',
+    USER_PATH: '/auth/user',
+  };
