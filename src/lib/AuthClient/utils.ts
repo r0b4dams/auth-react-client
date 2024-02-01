9c1eb67b5a09cfe226127b3dbb09c84c40b9a297
Robert Adams <r0b4dams@gmail.com>
2024-01-31 21:00:21 -0800

    Refactor

--- src/lib/AuthClient/utils.ts
+++ src/lib/AuthClient/utils.ts
@@ -1,9 +1,9 @@
-export function generateState(): string {
+const dec2hex = (d: number): string => {
+  return d.toString(16).padStart(2, "0");
+};
+
+export const generateState = (): string => {
   const array = new Uint32Array(10);
   window.crypto.getRandomValues(array);
   return Array.from(array, dec2hex).join("");
-}
-
-function dec2hex(d: number) {
-  return d.toString(16).padStart(2, "0");
-}
+};
