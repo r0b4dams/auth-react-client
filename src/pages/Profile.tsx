e0b7e20464ddd603bad940a80a61c5f2c6203356
Robert Adams <r0b4dams@gmail.com>
2024-01-31 21:53:08 -0800

    Update Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,3 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useFusionAuth, RequireAuth } from "@fusionauth/react-sdk";
+
 export const Profile: React.FC = (): JSX.Element => {
-  return <div>Profile</div>;
+  const { user, isAuthenticated } = useFusionAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <RequireAuth>
+      <div>Profile</div>;
+    </RequireAuth>
+  );
 };
