0d2ff2800834d5386284c1f05357bd0f9973d603
Robert Adams <r0b4dams@gmail.com>
2024-01-31 19:44:51 -0800

    Add AuthProvider and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
 import { AppRouter } from "./AppRouter";
+import { AuthProvider } from "./lib/AuthClient/AuthProvider";
+import { useAuth } from "./lib/AuthClient/hooks/useAuth";
+
+const auth_config = {
+  clientId: import.meta.env.VITE_FUSIONAUTH_CLIENT_ID,
+  serverUrl: import.meta.env.VITE_TOKEN_EXCHANGE_URL,
+  redirectUri: import.meta.env.VITE_TOKEN_EXCHANGE_REDIRECT_URL,
+};
 
 export const App = () => {
-  return <AppRouter />;
+  const { isLoading } = useAuth();
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setReady(true), 1500);
+  }, [isLoading]);
+
+  return (
+    <AuthProvider {...auth_config}>
+      <AppRouter />
+    </AuthProvider>
+  );
 };
