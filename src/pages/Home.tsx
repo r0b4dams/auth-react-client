7a84b7f7605990986ae9b26971c92c63e8cf3a02
Robert Adams <r0b4dams@gmail.com>
2024-01-31 20:59:58 -0800

    Add buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+import { Button } from "../components/Button";
+
 export const Home: React.FC = (): JSX.Element => {
-  return <div>Home</div>;
+  const handleSignup = () => {
+    console.log("signup");
+  };
+
+  const handleLogin = () => {
+    console.log("login");
+  };
+
+  return (
+    <div className="flex flex-col space-y-10">
+      <Button onClick={handleSignup}>Signup</Button>
+      <Button onClick={handleLogin}>Login</Button>
+    </div>
+  );
 };
