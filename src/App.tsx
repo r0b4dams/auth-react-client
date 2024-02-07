deb727dd69491cbdeb260e6893ac57622f83d955
Robert Adams <r0b4dams@gmail.com>
2024-02-07 12:22:24 -0800

    Add endpoint overrides to FusionAuthProvider config

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ const config = {
   clientID: import.meta.env.VITE_FUSIONAUTH_CLIENT_ID,
   serverUrl: import.meta.env.VITE_TOKEN_EXCHANGE_URL,
   redirectUri: import.meta.env.VITE_TOKEN_EXCHANGE_REDIRECT_URL,
+
+  loginPath: "/auth/login",
+  logoutPath: "/auth/logout",
+  registerPath: "/auth/register",
+  tokenRefreshPath: "/auth/refresh",
+  mePath: "/auth/me",
 };
 
 const _App_ = () => {
