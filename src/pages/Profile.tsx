2c3fd5e08e64a54911a44fe290ef5f700b8ee438
Robert Adams <r0b4dams@gmail.com>
2024-02-07 23:10:02 -0800

    Render profile data

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,7 @@
 import { Navigate } from "react-router-dom";
-import { useFusionAuth, RequireAuth } from "@fusionauth/react-sdk";
+import { useFusionAuth } from "@fusionauth/react-sdk";
+
+const imgFallback = "https://cdn-icons-png.flaticon.com/512/149/149071.png";
 
 export const Profile: React.FC = (): JSX.Element => {
   const { user, isAuthenticated } = useFusionAuth();
@@ -8,11 +10,24 @@ export const Profile: React.FC = (): JSX.Element => {
     return <Navigate to="/" />;
   }
 
-  console.log(user);
-
   return (
-    <RequireAuth>
-      <div>Profile</div>;
-    </RequireAuth>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <img
+        alt="avatar"
+        src={user.picture || imgFallback}
+        style={{ borderRadius: "9999px" }}
+        width={200}
+        height={200}
+      />
+
+      <div>{`${user.given_name} ${user.family_name}`}</div>
+      <div>{user.email}</div>
+    </div>
   );
 };
